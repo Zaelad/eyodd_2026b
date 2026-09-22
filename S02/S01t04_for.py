e034a6eb086ccc6327b69ad68be49fa7134cfabf
Zaelad <castilloruizuriel21@gmail.com>
2026-09-22 16:41:45 +0000

    ✨ s01t0401 for finaliza toma de tiempo de la suma de 100 numeros usando un for

--- S02/S01t04_for.py
+++ S02/S01t04_for.py
@@ -13,8 +13,22 @@ timestamp_01 = time.time()
 #Porgrama que calcula las suma
 #de los "n" numeros naturales
 n = 100
-sum = 0
+tottal_sum = 0
 
 #Ciclo for 
 for number in range(1,n+1):
-    print(str(number) + "")
+    tottal_sum = tottal_sum + number
+    # 1: sum <- 0 + 1
+    # sum = 1
+    # 2: sum <- 1 + 2
+    # sum = 3
+    # 3: sum <- 3 + 4
+    # ...
+    # 100: sum <- sum_(-1) + 100
+print(f"la suma de 1 hasta{n} es: {tottal_sum}")
+
+# tomando el tiempo final
+timestamp_02 = time.time()
+
+# impresion del tiempo de ejecucion
+print(f"tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6:.2f}  μs")
